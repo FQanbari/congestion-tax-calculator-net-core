44674009d5c0124f90fe7e867e2cc9aec1877585
Fatemeh <fatemeh.ganbari1993@gmail.com>
2023-12-02 19:21:32 +0330

    add tow tests for limitation

--- test/congestion-tax-calculator-net-core.Tests/CongestionTaxCalculatorTests.cs
+++ test/congestion-tax-calculator-net-core.Tests/CongestionTaxCalculatorTests.cs
@@ -99,5 +99,45 @@ namespace congestion_tax_calculator_net_core.Tests
             // Assert
             Assert.Equal(expectedFee, result);
         }
+        [Theory]
+        [InlineData(VehicleEnum.Car, "2013-02-08 06:00:27;2013-02-08 07:01:00;2013-02-08 08:03:00;2013-02-08 14:35:00;2013-02-08 15:29:00;2013-02-08 15:47:00;2013-02-08 16:01:00;2013-02-08 16:48:00;2013-02-08 17:49:00;2013-02-08 18:29:00;2013-02-08 18:35:00", 60)]
+        public void GetTax_ShouldReachMaxAmountPerDay_ReturnsMaxFee(VehicleEnum vehicle, string datesStr, int expectedFee)
+        {
+            // Arrange
+            IVehicle v = VehicleFactory.Create(vehicle);
+            var dateTimes = new List<DateTime>();
+            string[] dates = datesStr.Split(";");
+            foreach (string date in dates)
+            {
+                if (!string.IsNullOrWhiteSpace(date))
+                    dateTimes.Add(DateTime.Parse(date));
+            }
+
+            // Act
+            int result = calculator.GetTax(v, dateTimes.ToArray());
+
+            // Assert
+            Assert.Equal(expectedFee, result);
+        }
+        [Theory]
+        [InlineData(VehicleEnum.Car, "2013-02-08 14:35:00;2013-02-08 15:29:00", 13)]
+        public void GetTax_Passwithin60Min_ReturnMaxFee(VehicleEnum vehicle, string datesStr, int expectedFee)
+        {
+            // Arrange
+            IVehicle v = VehicleFactory.Create(vehicle);
+            var dateTimes = new List<DateTime>();
+            string[] dates = datesStr.Split(";");
+            foreach (string date in dates)
+            {
+                if (!string.IsNullOrWhiteSpace(date))
+                    dateTimes.Add(DateTime.Parse(date));
+            }
+
+            // Act
+            int result = calculator.GetTax(v, dateTimes.ToArray());
+
+            // Assert
+            Assert.Equal(expectedFee, result);
+        }
     }
 }
